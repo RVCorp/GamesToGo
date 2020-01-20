67ffe4bc869959747aaeb09b0b5b0ad2efc6693e
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2020-01-19 18:03:28 -0600

    Create window

---
+++ GamesToGo.Desktop/GamesToGoEditor.cs
@@ -0,0 +1,36 @@
+﻿using osu.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics.Performance;
+
+namespace GamesToGo.Desktop
+{
+    public class GamesToGoEditor : Game
+    {
+        private SpriteText text;
+
+        public GamesToGoEditor()
+        {
+            Name = "GamesToGo";
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(FrameworkConfigManager config)
+        {
+            config.GetBindable<WindowMode>(FrameworkSetting.WindowMode).Value = WindowMode.Borderless;
+
+            FrameStatistics.Value = FrameStatisticsMode.None;
+
+            Add(text = new SpriteText
+            {
+                Text = "Start",
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre
+            });
+        }
+
+
+    }
+}
--- GamesToGo.Desktop/Program.cs
+++ GamesToGo.Desktop/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using osu.Framework;
+using osu.Framework.Platform;
 
 namespace GamesToGo.Desktop
 {
     public static class Program
     {
         [STAThread]
-        public static void Main(string[] args) { }
+        public static void Main(string[] args)
+        {
+            using (DesktopGameHost host = Host.GetSuitableHost(@"GamesToGo"))
+            using (Game game = new GamesToGoEditor())
+                host.Run(game);
+        }
     }
 }
