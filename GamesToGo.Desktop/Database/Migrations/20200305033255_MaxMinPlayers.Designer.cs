f1bc25b04fb7b3d60bc7fddf48c8a3275022537c
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2020-03-04 21:34:49 -0600

    Add MaxMinPlayers migration

--- GamesToGo.Desktop/Database/Migrations/20200305033255_MaxMinPlayers.Designer.cs
+++ GamesToGo.Desktop/Database/Migrations/20200305033255_MaxMinPlayers.Designer.cs
@@ -2,14 +2,16 @@
 using GamesToGo.Desktop.Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GamesToGo.Desktop.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20200305033255_MaxMinPlayers")]
+    partial class MaxMinPlayers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
