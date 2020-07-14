064c22b79ea6c9ab461d9a7a335e59b646436569
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2020-07-13 23:45:32 -0500

    No constrains

--- GamesToGo.Desktop/Screens/ProjectHomeScreen.cs
+++ GamesToGo.Desktop/Screens/ProjectHomeScreen.cs
@@ -2,7 +2,6 @@ using System;
 using GamesToGo.Desktop.Graphics;
 using GamesToGo.Desktop.Project;
 using GamesToGo.Desktop.Project.Elements;
-using NUnit.Framework.Constraints;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
