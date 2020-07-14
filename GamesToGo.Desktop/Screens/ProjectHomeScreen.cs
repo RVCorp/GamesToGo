38bd04a9a92ef1b677f4cc931a6796b88c11a989
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2020-07-13 23:45:15 -0500

    Return of the GridContainers

--- GamesToGo.Desktop/Screens/ProjectHomeScreen.cs
+++ GamesToGo.Desktop/Screens/ProjectHomeScreen.cs
@@ -2,7 +2,9 @@ using System;
 using GamesToGo.Desktop.Graphics;
 using GamesToGo.Desktop.Project;
 using GamesToGo.Desktop.Project.Elements;
+using NUnit.Framework.Constraints;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
@@ -33,99 +35,129 @@ namespace GamesToGo.Desktop.Screens
                     RelativeSizeAxes = Axes.Both,
                     Colour = new Color4 (106,100,104, 255)      //Color fondo general
                 },
-                new Container
+                new GridContainer
                 {
-                    Anchor = Anchor.TopCentre,
-                    Origin = Anchor.TopCentre,
-                    RelativeSizeAxes = Axes.X,
-                    Height = 180,
-                    Children = new Drawable[]
-                    {
-                        new Box
-                        {
-                            RelativeSizeAxes = Axes.Both,
-                            Colour = Color4.Black
-                        },
-                        new SpriteText
-                        {
-                            Text = "Nombre del proyecto:",
-                            Position = new Vector2(15,17)
-                        },
-                        titleTextBox = new BasicTextBox
-                        {
-                            Text = project.DatabaseObject.Name,
-                            Position = new Vector2(175,10),
-                            Height = 35,
-                            Width = 775,
-                        },
-                        new SpriteText
-                        {
-                            Anchor = Anchor.TopCentre,
-                            Text = "Minimo Jugadores:",
-                            Position = new Vector2(560, 17)
-                        },
-                        minPlayersTextBox = new NumericTextbox        //Restringir la cantidad de digitos a 2 
-                        {
-                            Text = Math.Max(2, project.DatabaseObject.MinNumberPlayers).ToString(),
-                            Anchor = Anchor.TopCentre,
-                            Position = new Vector2(694, 10),
-                            Height = 35,
-                            Width = 50,
-                            CommitOnFocusLost = true,
-                        },
-                        new SpriteText
-                        {
-                            Anchor = Anchor.TopCentre,
-                            Text = "Maximo Jugadores:",
-                            Position = new Vector2(760, 17)
-                        },
-                        maxPlayersTextBox = new NumericTextbox        //Restringir la cantidad de digitos a 2 
-                        {
-                            Text = Math.Min(32, project.DatabaseObject.MaxNumberPlayers).ToString(),
-                            Anchor = Anchor.TopCentre,
-                            Position = new Vector2(898, 10),
-                            Height = 35,
-                            Width = 50,
-                            CommitOnFocusLost = true,
-                        },
-                        new SpriteText
-                        {
-                            Text = "Descripción:",
-                            Position = new Vector2(80,70)
-                        },
-                        new BasicTextBox        //Textbox de varios renglones
-                        {
-                            Position = new Vector2(175,70),
-                            Height = 100,
-                            Width = 1732
-                        }
-                    }
-                },
-                new Container
-                {
-                    Padding = new MarginPadding { Top = 180 },
                     RelativeSizeAxes = Axes.Both,
-                    Children = new Drawable[]
+                    Content = new Drawable[][]
                     {
-                        new ProjectObjectManagerContainer<Card>("Cartas")
+                        new Drawable[]
                         {
-                            Anchor = Anchor.BottomLeft,
-                            Origin = Anchor.BottomLeft,
-                            Width = 1/3f,
+                            new Container
+                            {
+                                AutoSizeAxes = Axes.Y,
+                                RelativeSizeAxes = Axes.X,
+                                Children = new Drawable[]
+                                {
+                                    new Container
+                                    {
+                                        Anchor = Anchor.TopCentre,
+                                        Origin = Anchor.TopCentre,
+                                        RelativeSizeAxes = Axes.X,
+                                        Height = 180,
+                                        Children = new Drawable[]
+                                        {
+                                            new Box
+                                            {
+                                                RelativeSizeAxes = Axes.Both,
+                                                Colour = Color4.Black.Opacity(0.8f),
+                                            },
+                                            new SpriteText
+                                            {
+                                                Text = "Nombre del proyecto:",
+                                                Position = new Vector2(15,17)
+                                            },
+                                            titleTextBox = new BasicTextBox
+                                            {
+                                                Text = project.DatabaseObject.Name,
+                                                Position = new Vector2(175,10),
+                                                Height = 35,
+                                                Width = 775,
+                                            },
+                                            new SpriteText
+                                            {
+                                                Anchor = Anchor.TopCentre,
+                                                Text = "Minimo Jugadores:",
+                                                Position = new Vector2(560, 17)
+                                            },
+                                            minPlayersTextBox = new NumericTextbox        //Restringir la cantidad de digitos a 2 
+                                            {
+                                                Text = Math.Max(2, project.DatabaseObject.MinNumberPlayers).ToString(),
+                                                Anchor = Anchor.TopCentre,
+                                                Position = new Vector2(694, 10),
+                                                Height = 35,
+                                                Width = 50,
+                                                CommitOnFocusLost = true,
+                                            },
+                                            new SpriteText
+                                            {
+                                                Anchor = Anchor.TopCentre,
+                                                Text = "Maximo Jugadores:",
+                                                Position = new Vector2(760, 17)
+                                            },
+                                            maxPlayersTextBox = new NumericTextbox        //Restringir la cantidad de digitos a 2 
+                                            {
+                                                Text = Math.Min(32, project.DatabaseObject.MaxNumberPlayers).ToString(),
+                                                Anchor = Anchor.TopCentre,
+                                                Position = new Vector2(898, 10),
+                                                Height = 35,
+                                                Width = 50,
+                                                CommitOnFocusLost = true,
+                                            },
+                                            new SpriteText
+                                            {
+                                                Text = "Descripción:",
+                                                Position = new Vector2(80,70)
+                                            },
+                                            new BasicTextBox        //Textbox de varios renglones
+                                            {
+                                                Position = new Vector2(175,70),
+                                                Height = 100,
+                                                Width = 1732
+                                            }
+                                        }
+                                    },
+                                }
+                            }
                         },
-                        new ProjectObjectManagerContainer<Token>("Fichas")
+                        new Drawable[]
                         {
-                            Anchor = Anchor.BottomCentre,
-                            Origin = Anchor.BottomCentre,
-                            Width = 1/3f,
-                        },
-                        new ProjectObjectManagerContainer<Board>("Tableros")
-                        {
-                            Anchor = Anchor.BottomRight,
-                            Origin = Anchor.BottomRight,
-                            Width = 1/3f,
-                        },
-                    }
+                            new Container
+                            {
+                                RelativeSizeAxes = Axes.Both,
+                                Width = 0.5f,
+                                Children = new Drawable[]
+                                {
+                                    new ProjectObjectManagerContainer<Card>("Cartas")
+                                    {
+                                        Anchor = Anchor.BottomLeft,
+                                        Origin = Anchor.BottomLeft,
+                                        Width = 1/3f,
+                                    },
+                                    new ProjectObjectManagerContainer<Token>("Fichas")
+                                    {
+                                        Anchor = Anchor.BottomCentre,
+                                        Origin = Anchor.BottomCentre,
+                                        Width = 1/3f,
+                                    },
+                                    new ProjectObjectManagerContainer<Board>("Tableros")
+                                    {
+                                        Anchor = Anchor.BottomRight,
+                                        Origin = Anchor.BottomRight,
+                                        Width = 1/3f,
+                                    },
+                                }
+                            }
+                        }
+                    },
+                    ColumnDimensions = new Dimension[]
+                    {
+                        new Dimension(),
+                    },
+                    RowDimensions = new Dimension[]
+                    {
+                        new Dimension(GridSizeMode.AutoSize),
+                        new Dimension()
+                    },
                 }
             };
 
