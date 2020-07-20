3f3234729101a1608aef365f65f13343bcff1c08
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2020-07-20 02:33:00 -0500

    Remove unused using Not using usings no more

--- GamesToGo.Desktop/Overlays/ImageFinderOverlay.cs
+++ GamesToGo.Desktop/Overlays/ImageFinderOverlay.cs
@@ -10,7 +10,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Logging;
 using osu.Framework.Platform;
 using osuTK;
