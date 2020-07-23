227564610eb392e490d5ab1768df7089432e6225
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2020-07-22 21:17:59 -0500

    Add Online Framework ISPs hate him! Check this one click to go online!

---
+++ GamesToGo.Desktop/Online/User.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace GamesToGo.Desktop.Online
+{
+    public class User
+    {
+        [JsonProperty(@"Id")]
+        public int ID { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonProperty(@"UsertypeId")]
+        public int UserTypeID { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
