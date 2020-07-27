b8cf933412b999ca289062304b246d3cc1d6fc61
Diego <driveror31@gmail.com>
2020-07-27 02:27:20 -0500

    Look Mom, I Apported to the Desktop App

---
+++ GamesToGo.Desktop/Online/GetProjectRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesToGo.Desktop.Online
+{
+    public class GetProjectRequest : APIRequest<Project>
+    {
+        private int gameID;
+        public GetProjectRequest(int id)
+        {
+            gameID = id;
+        }
+        protected override string Target => $"games/{gameID}";
+    }
+}
