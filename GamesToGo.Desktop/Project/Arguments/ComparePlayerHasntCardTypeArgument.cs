788b2c56b3105ad1176ce06a0b70b80c7ed55d8d
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2020-10-12 02:47:26 -0500

    Come up with namespaces for namespaced stuff

--- GamesToGo.Desktop/Project/Arguments/ComparePlayerHasntCardTypeArgument.cs
+++ GamesToGo.Desktop/Project/Arguments/ComparePlayerHasntCardTypeArgument.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GamesToGo.Desktop.Project.Events.Arguments
+﻿namespace GamesToGo.Desktop.Project.Arguments
 {
     public class ComparePlayerHasntCardTypeArgument : Argument
     {
