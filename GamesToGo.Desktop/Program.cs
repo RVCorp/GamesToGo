aff67b6e75f194864bcded1af2f0abe62406f272
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2020-11-21 15:28:54 -0600

    Allow using command line argument to test local server
    usage: --server=http://your.local.server:port

--- GamesToGo.Desktop/Program.cs
+++ GamesToGo.Desktop/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using GamesToGo.Desktop.Online;
 using osu.Framework;
 using osu.Framework.Platform;
 
@@ -7,8 +9,11 @@ namespace GamesToGo.Desktop
     public static class Program
     {
         [STAThread]
-        public static void Main()
+        public static void Main(params string[] args)
         {
+            string server;
+            if ((server = args.FirstOrDefault(a => a == @"--server=")) != null)
+                APIController.AlternativeServer = server.Substring(server.IndexOf('='));
             //Estas tres lineas crean la ventana base del proyecto (en el lenguaje del framework, un "juego"), y crean una ventana para el
             using DesktopGameHost host = Host.GetSuitableHost(@"GamesToGo");
             using Game program = new GamesToGoEditor();
