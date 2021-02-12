05af88923062a3b71c71a6ac7f98150287947f56
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2021-02-11 18:09:30 -0600

    Improve SurfaceButton to allow a background colour

--- GamesToGo.Game/Graphics/SurfaceButton.cs
+++ GamesToGo.Game/Graphics/SurfaceButton.cs
@@ -1,11 +1,44 @@
 ﻿using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.UserInterface;
 
 namespace GamesToGo.Game.Graphics
 {
     public class SurfaceButton : Button
     {
+        /// <summary>
+        /// Background colour for this button. <see cref="Colour4.Transparent"/> by default.
+        /// </summary>
+        public ColourInfo BackgroundColour
+        {
+            get => backgroundBox.Colour;
+            set => backgroundBox.Colour = value;
+        }
+
+        private readonly Container content;
+        private readonly Box backgroundBox;
+
+        protected override Container<Drawable> Content => content;
+
+        public SurfaceButton()
+        {
+            InternalChildren = new Drawable[]
+            {
+                backgroundBox = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Colour4.Transparent,
+                },
+                content = new Container
+                {
+                    RelativeSizeAxes = Axes.Both,
+                },
+            };
+        }
+
         [BackgroundDependencyLoader]
         private void load()
         {
