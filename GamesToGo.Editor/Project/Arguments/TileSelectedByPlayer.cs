a03c07d87e591be074b7ed0ae2e23f51b3e775ff
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2021-05-03 00:33:18 -0500

    Add requested actions and arguments
    Salt is useful

---
+++ GamesToGo.Editor/Project/Arguments/TileSelectedByPlayer.cs
@@ -0,0 +1,22 @@
+﻿using GamesToGo.Common.Game;
+using JetBrains.Annotations;
+
+namespace GamesToGo.Editor.Project.Arguments
+{
+    [UsedImplicitly]
+    public class TileSelectedByPlayer : Argument
+    {
+        public override int ArgumentTypeID => 24;
+        public override ArgumentReturnType Type => ArgumentReturnType.SingleTile;
+
+        public override ArgumentReturnType[] ExpectedArguments => new[]
+        {
+            ArgumentReturnType.SinglePlayer,
+        };
+
+        public override string[] Text { get; } =
+        {
+            @"Casilla seleccionada por jugador",
+        };
+    }
+}
